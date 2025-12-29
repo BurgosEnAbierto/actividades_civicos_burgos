46eccf5008ddee52b843e44e1ff530928c62a4d0
Kowagunga <kowagunga@gmail.com>
2025-12-30 00:02:04 +0100

    feat: start project rafactoring and workflow
    - Readme
    - Version
    - JSON schemas
    - civicos.json
    - json validator script

---
+++ src/version.py
@@ -0,0 +1,5 @@
+from pathlib import Path
+
+def get_version() -> str:
+    version_file = Path(__file__).resolve().parent.parent / "VERSION"
+    return version_file.read_text(encoding="utf-8").strip()
